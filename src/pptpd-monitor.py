7689ad79cecb6f119267d5e9632f0af97050f82b
DanKliuiev <dankliuiev@planeks.net>
2020-03-21 10:21:53 +0200

    --watch without --delay is now actually working

--- src/pptpd-monitor.py
+++ src/pptpd-monitor.py
@@ -290,7 +290,7 @@ if __name__ == "__main__":
 
   monitor = Monitor(logfile, logrotate)
 
-  if args.watch:
+  if args.watch and args.delay:
     monitor.monitor(interval=args.delay)
   else:
     monitor.monitor()
